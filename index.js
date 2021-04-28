661063f88750c607c47572f915c3eb8aac317e87
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 22:17:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+const findMatching = (driversArray, string) => {
+  return driversArray.filter (n => n.toLowerCase() === string.toLowerCase())  //returns the matching list of drivers.
+                                                                              //The function should be case insensitive.
+}
+
+const fuzzyMatch = (driversArray, string) => {
+  return driversArray.filter (n => n.slice(0,string.length) === string)    //returns all drivers whose names begin with the provided letters
+}
+
+const matchName = (driversArray, string) => {
+  return driversArray.filter (n => n.name === string)     //return each element whose name property matches the provided string argument
+}
