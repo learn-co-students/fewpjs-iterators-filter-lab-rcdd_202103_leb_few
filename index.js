6f11983dbd0da15baea43eeea3c3e466fd0741e3
wadad zein <wadad_zein@hotmail.com>
2021-04-29 19:11:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+ const fuzzyMatch = (array, string) => {
+  return array.filter(word => word.slice(0,string.length) === string);
+};
+
+const findMatching = (array, string) => {
+  return array.filter(word=>word.toUpperCase()===string.toUpperCase());
+};
+
+ const matchName = (array, string) => {
+   return array.filter(word=>word.name===string);
+ };
