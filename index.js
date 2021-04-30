406b0d6520465a1cc2f35e0c93c6e38c8f2374b0
Cesar Maaz <cesarmaaz@gmail.com>
2021-04-30 07:29:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+// find the matching name in a list of names when case-sensitivity doesn't matter
+function findMatching (list, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+// find the partial match of a name in a list of names when case_sensitivity doesn't matter
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
+// match values of an Obj to a provided string
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
