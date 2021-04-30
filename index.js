a40b0a455df5a2b1bc0d946343a3714fee8af607
Salim Al Mersally <salim.almersally@lau.edu>
2021-04-30 20:42:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+  return drivers.filter(function(driver){
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string){
+  let letter = string.charAt(0);
+  return drivers.filter(function(driver){
+    return driver.charAt(0) === letter;
+  });
+}
+
+function matchName(drivers, string){
+  return drivers.filter(function(driver){
+    return driver.name === string;
+  });
+}
